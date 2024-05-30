64e2280bb5424d1c4b961e682098b476c33f7135
Eduardo <eduardo.maciel@edu.univali.br>
2024-05-30 19:12:56 -0300

    Remove js build and add .gitkeep

--- src/core/services/supabase-service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.supabaseService = void 0;
-require("dotenv/config");
-const supabase_js_1 = require("@supabase/supabase-js");
-const SUPABASE_URL = process.env.SUPABASE_URL;
-const SUPABASE_KEY = process.env.SUPABASE_KEY;
-const supabaseService = (0, supabase_js_1.createClient)(SUPABASE_URL, SUPABASE_KEY);
-exports.supabaseService = supabaseService;
--- src/core/utils/delay.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.delay = void 0;
-const delay = (t, v) => {
-    return new Promise((resolve) => setTimeout(() => resolve(v), t));
-};
-exports.delay = delay;
